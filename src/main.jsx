385846bcdf4984453fb3b23197833d9959491008
Aleksey Kuznetsov <kuznetcov.alekcey@icloud.com>
2025-03-14 14:57:17 +0300

    Initial root of application.

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client";
+import App from "./components/app/App.jsx";
+import "./main.css"
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(<App />)
